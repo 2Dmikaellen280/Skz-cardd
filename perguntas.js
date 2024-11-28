76e221c2701f1f0b52a88ca5bf1f4335ed9adc0b
2Dmikaellen280 <mikolibar@gmail.com>
2024-11-28 14:03:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'membros',
+    'Quais são as rachas oficias do Stray Kids?',
+    '3 RACHA, Dance Racha e Vocal Racha'
+)
+
+criaCartao(
+    'Lee know',
+    'Qual o nome dos gatinhos do Lee know?',
+    'Soonie, Doongie e Dori'
+)
+
+criaCartao(
+    'Show',
+    'O Stray kids anúnciou show no Brasil, quais são as datas e os locais?',
+    ' Rio de Janeiro no dia 01/04/2025 e em São Paulo no dia 05/04/2025, com chances de de abrir outras datas se der sold out '
+)
+
+criaCartao(
+    'Stray Kids',
+    'Qual a data de debut do Stray Kids?',
+    '25 de Março de 2018'
+)
